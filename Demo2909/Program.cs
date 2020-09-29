dec982c853bf83238a81ae869e0f61d418d76322
WimHambrouck <wim.hambrouck@gmail.com>
2020-09-29 10:44:53 +0200

    Add project files.

---
+++ Demo2909/Program.cs
@@ -0,0 +1,18 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+
+namespace Demo2909
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lijst = new List<string> { "een", "twee", "drie", "vier" };
+
+            lijst.Remove("twee");
+
+            Console.WriteLine(string.Join(", ", lijst));
+        }
+    }
+}
---
+++ DemoLibrary/Persoon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DemoLibrary
+{
+    public class Persoon
+    {
+        public string Voornaam { get; set; }
+        public string Achternaam { get; set; }
+
+        public override string ToString()
+        {
+            return Voornaam + " " + Achternaam;
+        }
+    }
+}
